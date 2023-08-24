edf08fb2c2173206a04f122f4ddd77b52de12936
leeass <ilua-smirnof@yandex.ru>
2023-08-24 19:27:06 +0200

    feat(Trainer): Basic note training game

--- src/components/trainer/trainer.tsx
+++ src/components/trainer/trainer.tsx
@@ -1,5 +1,65 @@
+import { useState } from 'react';
+import './trainer.css';
+import { Note, notes } from '../../constants/notes.const';
+
 export default function Trainer() {
+  const [currentNote, setCurrentNote] = useState<Note | null>(null);
+  const [userGuess, setUserGuess] = useState<Note | null>(null);
+
+  /**
+   * Return a random note from the notes array.
+   * @returns 
+   */
+  const generateRandomNote = () => {
+    let randomIndex = Math.floor(Math.random() * notes.length);
+    // Prevent same note generation
+    while (notes[randomIndex] === currentNote) {
+      randomIndex = Math.floor(Math.random() * notes.length);
+    }
+    return notes[randomIndex];
+  };
+
+  /**
+   * @desc If the guess is correct, generate next note.
+   * @event click
+   * @param note 
+   */
+  const handleNoteButtonClick = (note: Note) => {
+    setUserGuess(note);
+    if (userGuess === currentNote) {
+      setUserGuess(null);
+      setCurrentNote(generateRandomNote());
+    }
+  };
+
+  // Initial setup
+  if (currentNote === null) {
+    setCurrentNote(generateRandomNote());
+  }
+
   return (
-    <div>trainer</div>
-  )
+    <>
+      <section className="partition">
+        <div className={`note ${currentNote}`} />
+        <div className="lines">
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+        </div>
+      </section>
+      <p>Guess the note:</p>
+      <div>
+        {notes.map((note) => (
+          <button
+            key={note}
+            onClick={() => handleNoteButtonClick(note)}
+          >
+            {note}
+          </button>
+        ))}
+      </div>
+    </>
+  );
 }
--- src/constants/notes.const.ts
+++ src/constants/notes.const.ts
@@ -1,4 +1,9 @@
 /**
+ * Note type
+ */
+export type Note = 'do' | 're' | 'mi' | 'fa' | 'sol' | 'la' | 'si' | 'do'; 
+
+/**
  * The notes array
  */
-export const notes = ['do', 're', 'mi', 'fa', 'sol', 'la', 'si'] as const;
+export const notes: readonly Note[] = ['do', 're', 'mi', 'fa', 'sol', 'la', 'si'] as const;
