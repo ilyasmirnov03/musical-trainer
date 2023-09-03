da0ed42181fe5ab6f3fdadb90ea123cb81e61794
leeass <ilua.smirnof@gmail.com>
2023-09-03 15:06:55 +0200

    feat(Notes): Define notes initial position

--- src/constants/notes.const.ts
+++ src/constants/notes.const.ts
@@ -1,9 +1,42 @@
-/**
- * Note type
- */
-export type Note = 'do' | 're' | 'mi' | 'fa' | 'sol' | 'la' | 'si' | 'do'; 
+import { Note } from "../models/notes/notes.interface";
 
 /**
  * The notes array
  */
-export const notes: readonly Note[] = ['do', 're', 'mi', 'fa', 'sol', 'la', 'si'] as const;
+export const notes: readonly Note[] = [
+    {
+        name: 'do',
+        altPosition: -2,
+        bassPosition: 3
+    },
+    {
+        name: 're',
+        altPosition: -1,
+        bassPosition: 4
+    },
+    {
+        name: 'mi',
+        altPosition: 0,
+        bassPosition: 5
+    },
+    {
+        name: 'fa',
+        altPosition: 1,
+        bassPosition: 6
+    },
+    {
+        name: 'sol',
+        altPosition: 2,
+        bassPosition: 7
+    },
+    {
+        name: 'la',
+        altPosition: 3,
+        bassPosition: 8
+    },
+    {
+        name: 'si',
+        altPosition: 4,
+        bassPosition: 9
+    }
+] as const;
